78cb157662b968284f884200e2a1487946f87a51
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-06 10:44:06 +0100

    Events showing photo, title and description

--- src/componentes/Evento.js
+++ src/componentes/Evento.js
@@ -2,9 +2,32 @@ import React from 'react';
 
 const Evento = (props) => {
 
-    console.log(props.info);
+    const {name} = props.info;
+
+    if (!name) return null;
+
+    let desc = props.info.description.text;
+
+    if (desc.length > 250) {
+        desc = desc.substr(0, 250);
+    }
+
     return (
-        <p>desde evento.js</p>
+        <div>
+            <div className="uk-card uk-card-default">
+                <div className="uk-card-media-top">
+                    {props.info.logo !== null
+                        ?
+                        <img src={props.info.logo.url} alt={props.info.name.info}/>
+                        : ''
+                    }
+                </div>
+                <div className="uk-card-body">
+                    <h3 className="uk-card-title">{props.info.name.text}></h3>
+                    <p>{desc}</p>
+                </div>
+            </div>
+        </div>
     )
 };
 
