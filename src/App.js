4a68e040e58698324cc7134bccd8325eefbfa3e0
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-06 08:30:55 +0100

    Using API url to show through console all the categories

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React, {Component} from 'react';
 import Header from "./componentes/Header";
 
 class App extends Component {
+
+    componentDidMount() {
+        this.obtenerCategorias();
+    }
+
+    obtenerCategorias = async () => {
+        let url = `https://www.eventbriteapi.com/v3/categories/?token=EH7Z24DKZZBOLOBDL56D&locale=es_ES`;
+
+        await fetch(url)
+            .then(respuesta => {
+                return respuesta.json();
+            })
+            .then(categorias => {
+                console.log(categorias.categories);
+            })
+    };
+
     render() {
         return (
             <div className="App">
