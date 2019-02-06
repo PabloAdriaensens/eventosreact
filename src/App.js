3d86e83b60754f089497c3ed0e15c69bce0c3580
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-06 08:33:50 +0100

    Saving the categories in the state

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import Header from "./componentes/Header";
 
 class App extends Component {
 
+    token = 'EH7Z24DKZZBOLOBDL56D';
+
+    state = {
+        categorias: []
+    };
+
     componentDidMount() {
         this.obtenerCategorias();
     }
 
     obtenerCategorias = async () => {
-        let url = `https://www.eventbriteapi.com/v3/categories/?token=EH7Z24DKZZBOLOBDL56D&locale=es_ES`;
+        let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale=es_ES`;
 
         await fetch(url)
             .then(respuesta => {
                 return respuesta.json();
             })
             .then(categorias => {
-                console.log(categorias.categories);
+                this.setState({
+                    categorias: categorias.categories
+                })
             })
     };
 
